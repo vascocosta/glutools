200316529c46b0060190647ece85c711a345a5d2
Vasco Costa <vascomacosta@gmail.com>
2025-02-19 15:00:12 +0000

    Refactor

--- zig/remind/build.zig
+++ zig/remind/build.zig
@@ -1,22 +1,19 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    const target = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{});
+    const clap = b.dependency("clap", .{});
     const exe = b.addExecutable(.{
         .name = "remind",
         .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
+        .target = b.standardTargetOptions(.{}),
+        .optimize = b.standardOptimizeOption(.{}),
     });
-    const clap = b.dependency("clap", .{});
     exe.root_module.addImport("clap", clap.module("clap"));
     b.installArtifact(exe);
+
     const run_cmd = b.addRunArtifact(exe);
     run_cmd.step.dependOn(b.getInstallStep());
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
+    if (b.args) |args| run_cmd.addArgs(args);
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 }
